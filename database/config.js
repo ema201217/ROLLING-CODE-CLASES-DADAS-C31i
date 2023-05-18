ca21e22499c46ffdb6f8539c701e5a7e3a2612e8
Emanuel Arroyo <ema201217@gmail.com>
2023-05-18 20:09:17 -0300

    agrego variable de entorno MONGO_URL_ATLAS

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 module.exports = async () => {
   try {
-    const {connection} = await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+    const {connection} = await mongoose.connect(process.env.MONGO_URL_ATLAS);
     const {port, host} = connection
     console.log(`Conexión de mongoDB establecida -> ${host}:${port}`)
   } catch (error) {
